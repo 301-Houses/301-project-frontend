bde9d51da1227c07604b90cbe2f03e0fa9a99a9b
Nour Alahmad <Nour.Alahmad1@outlook.com>
2022-09-11 20:52:06 +0300

    creat ads $ whyUs sections

---
+++ src/Components/PremiumAds/PremiumAds.js
@@ -0,0 +1,35 @@
+import React from "react";
+import img1 from "./1403962.jpg";
+import "./PremiumAds.css";
+
+
+class PremiumAds extends React.Component {
+  render() {
+    return (
+      <div className="a" style={{ padding: "100px" }}>
+        <div className="card">
+          <div>
+            <div>
+              <img className="myimg" src={img1} alt="Logo" />
+            </div>
+            <div>
+
+            </div>
+            <div className="des">
+              <p>1200$ | 3dbs | 2ba - for sell  </p>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+            maximus, nulla ut commodo sagittis, sapien dui mattis dui, non
+           
+          </p>
+              
+            </div>
+          
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PremiumAds;
