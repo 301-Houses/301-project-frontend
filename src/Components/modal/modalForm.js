1504d2fbd74eba8b86091dfc6b4bc8805d53bbf5
monther123c <monthertamimi123@gmail.com>
2022-09-12 19:05:25 +0300

    modalForm

--- src/Components/modal/modalForm.js
+++ src/Components/modal/modalForm.js
@@ -6,37 +6,38 @@ class modal extends React.Component {
     render() {
       return (
         <Modal.Dialog  className="dialog">
-   <Modal.Header closeButton className="header" />
+  <Modal.Header closeButton className="header" />
 
 <Modal.Body className="mBody">
 <form className="form">
-    <input type="text" id="addres" name="address" ></input>
-    <label for="addres" > Address</label><br/>
-
-    <input type="text" id="HSize" name="size" ></input>
-    <label for="HSize" > House Size</label><br/>
-
-    <input type="text" id="NRooms" name="Rooms" ></input>
-    <label for="NRooms" > Number Of Rooms</label><br/>
-
-    <input type="text" id="Price" name="HPrice" ></input>
-    <label for="Price" > Price </label><br/>
-
-     <input type="text" id="PhoneN" name="PhoneN" ></input>
-    <label for="PhoneN" > Phone Number </label><br/>
+<label for="addres" > Address</label>
+    <input type="text" id="addres" name="address" ></input><br/>
+   
+    <label for="HSize" > House Size</label>
+    <input type="text" id="HSize" name="size" ></input><br/>
     
-    <input type="radio" id="isPremium" name="Premium" ></input>
-    <label for="isPremium" > Is Premium </label><br/>
+    <label for="NRooms" > Number Of Rooms</label>
+    <input type="text" id="NRooms" name="Rooms" ></input><br/>
+   
+    <label for="Price" > Price </label><br/>
+    <input type="text" id="Price" name="HPrice" ></input><br/>
 
-    <input type="radio" id="status" name="Status" ></input>
-    <label for="status" > Status</label><br/>
+    <label for="PhoneN" > Phone Number </label>
+     <input type="text" id="PhoneN" name="PhoneN" ></input><br/>
+   
+     <label  class="rad-label" for="isPremium" > Is Premium </label>
+    <input class="rad-input" type="radio" id="isPremium" name="Premium" ></input><br/>
+   
+    <label  class="rad-label" for="status" > Status</label>
+    <input class="rad-input" type="radio" id="status" name="Status" ></input><br/>
+   
     
     
     </form>
 </Modal.Body>
 <Modal.Footer className="footer">
 
-  <Button  type="submit" form="form1" value="Submit">Save changes</Button>
+  <Button className="bb"  type="submit" form="form1" value="Submit">Save changes</Button>
 </Modal.Footer>
       </Modal.Dialog>
     );
