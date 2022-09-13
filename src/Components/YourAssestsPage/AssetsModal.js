a51cb08d334f30543b0a6f4831d7885c7c29b834
islam alghoul <islamalghoul1997>
2022-09-13 18:39:54 +0300

    frontend

---
+++ src/Components/YourAssestsPage/AssetsModal.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+export class AssetsModal extends Component  {
+  render() {
+    return (
+        <Modal size="lg" show={this.props.show} onHide={this.props.handelCloseOnHide} >
+        <Modal.Header closeButton>
+        <Modal.Title>{this.props.modalData.status}</Modal.Title>
+         
+        </Modal.Header>
+        <Modal.Body> 
+        <form className="form" onSubmit={this.props.handelSubmite}>
+        <label for="imgURL" > imgURL</label>
+    <input type="text" id="imgURL" name="imgURL" defaultValue={this.props.modalData.imgURL}></input><br/>
+    
+<label for="ownerName" > Owner Name</label>
+    <input type="text" id="ownerName" name="ownerName" defaultValue={this.props.modalData.ownerName}></input><br/>
+   
+    <label for="houseAddress" > House Address</label>
+    <input type="text" id="houseAddress" name="houseAddress" defaultValue={this.props.modalData.houseAddress} ></input><br/>
+    
+    <label for="houseSize" > House Size </label><br/>
+    <input type="text" id="houseSize" name="houseSize" defaultValue={this.props.modalData.houseSize}></input><br/>
+
+    <label for="numOfRoomse" > Number Of Rooms </label>
+     <input type="text" id="numOfRoomse" name="numOfRoomse" defaultValue={this.props.modalData.numOfRoomse}></input><br/>
+   
+     <label for="price" > Price </label>
+     <input type="text" id="price" name="price" defaultValue={this.props.modalData.price}></input><br/>
+
+     <label for="phoneNumber" >Phone Number </label>
+     <input type="text" id="phoneNumber" name="phoneNumber" defaultValue={this.props.modalData.phoneNumber}></input><br/>
+
+     <input type="checkbox" id="ispremium" name="ispremium" ></input>
+<label for="ispremium"> Premium</label><br/>
+
+<input onClick={this.props.handleChange} type="radio" id="forRent" name="status" ></input>
+<label for="status">for Rent</label><br/>
+<input onClick={this.props.handleChange} type="radio" id="forSale" name="status" ></input>
+<label  for="status"> for Sale</label><br/>
+
+
+    <button> Submit</button>
+    </form>
+    </Modal.Body>
+        
+        <Modal.Footer>
+       
+        
+          
+        </Modal.Footer>
+      </Modal>
+      
+    )
+  }
+}
+export default AssetsModal;
+
+
--- src/Components/YourAssestsPage/YourAssets.js
+++ src/Components/YourAssestsPage/YourAssets.js
@@ -1,14 +1,37 @@
+import axios from "axios";
 import React from "react";
+import { withAuth0 } from "@auth0/auth0-react";
+import AssetsCards from "./AssetsCards";
 
 class YourAssests extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+userData:[]
+    }
+    
+}
+componentDidMount=()=>{
+  const {  user } = this.props.auth0;
+ let email=user.email
+  axios.get('http://localhost:3001/house/test2@gmail.com').then(result=>{
+
+  this.setState({
+    userData:result.data
+  })
+  console.log(this.state.userData)
+  })
+  
+
+  }
   render() {
     return (
       <>
-        <h1>User Assets Page</h1>
-        <h1>Editable Cards</h1>
+      <AssetsCards userData={this.state.userData} />
+      
       </>
     );
   }
 }
 
-export default YourAssests;
+export default withAuth0(YourAssests);
