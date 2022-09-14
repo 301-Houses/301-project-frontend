76fda7d2381fde617f565717aeae46b7cf38347c
monther123c <monthertamimi123@gmail.com>
2022-09-14 20:19:15 +0300

    last day

--- src/Components/YourAssestsPage/AssetsModal.js
+++ src/Components/YourAssestsPage/AssetsModal.js
@@ -1,58 +1,65 @@
 import React, { Component } from 'react'
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import "./modalForm.css";
 
-export class AssetsModal extends Component  {
+export class AssetsModal extends Component {
   render() {
     return (
-        <Modal size="lg" show={this.props.show} onHide={this.props.handelCloseOnHide} >
+      <Modal size="lg" show={this.props.show} onHide={this.props.handelCloseOnHide} >
         <Modal.Header closeButton>
-        <Modal.Title>{this.props.modalData.status}</Modal.Title>
-         
+          <Modal.Title>{this.props.modalData.status}</Modal.Title>
+
         </Modal.Header>
-        <Modal.Body> 
-        <form className="form" onSubmit={this.props.handelSubmite}>
-        <label for="imgURL" > imgURL</label>
-    <input type="text" id="imgURL" name="imgURL" defaultValue={this.props.modalData.imgURL}></input><br/>
-    
-<label for="ownerName" > Owner Name</label>
-    <input type="text" id="ownerName" name="ownerName" defaultValue={this.props.modalData.ownerName}></input><br/>
-   
-    <label for="houseAddress" > House Address</label>
-    <input type="text" id="houseAddress" name="houseAddress" defaultValue={this.props.modalData.houseAddress} ></input><br/>
-    
-    <label for="houseSize" > House Size </label><br/>
-    <input type="text" id="houseSize" name="houseSize" defaultValue={this.props.modalData.houseSize}></input><br/>
+        <Modal.Body>
+          <div className='assets'>
+            <form className="assetsForm" onSubmit={this.props.handelSubmite}>
+              <label for="imgURL" > imgURL</label>
+              <input type="text" id="imgURL" name="imgURL" defaultValue={this.props.modalData.imgURL}></input>
 
-    <label for="numOfRoomse" > Number Of Rooms </label>
-     <input type="text" id="numOfRoomse" name="numOfRoomse" defaultValue={this.props.modalData.numOfRoomse}></input><br/>
-   
-     <label for="price" > Price </label>
-     <input type="text" id="price" name="price" defaultValue={this.props.modalData.price}></input><br/>
+              <label for="ownerName" > Owner Name</label>
+              <input type="text" id="ownerName" name="ownerName" defaultValue={this.props.modalData.ownerName}></input>
 
-     <label for="phoneNumber" >Phone Number </label>
-     <input type="text" id="phoneNumber" name="phoneNumber" defaultValue={this.props.modalData.phoneNumber}></input><br/>
+              <label for="houseAddress" > House Address</label>
+              <input type="text" id="houseAddress" name="houseAddress" defaultValue={this.props.modalData.houseAddress} ></input>
 
-     <input type="checkbox" id="ispremium" name="ispremium" ></input>
-<label for="ispremium"> Premium</label><br/>
+              <label for="houseSize" > House Size </label>
+              <input type="text" id="houseSize" name="houseSize" defaultValue={this.props.modalData.houseSize}></input>
 
-<input onClick={this.props.handleChange} type="radio" id="forRent" name="status" value="forRent" ></input>
-<label for="status">for Rent</label><br/>
-<input onClick={this.props.handleChange} type="radio" id="forSale" name="status" value="forSale" ></input>
-<label  for="status"> for Sale</label><br/>
+              <label for="numOfRoomse" > Number Of Rooms </label>
+              <input type="text" id="numOfRoomse" name="numOfRoomse" defaultValue={this.props.modalData.numOfRoomse}></input>
+
+              <label for="price" > Price </label>
+              <input type="text" id="price" name="price" defaultValue={this.props.modalData.price}></input>
+
+              <label for="phoneNumber" >Phone Number </label>
+              <input type="text" id="phoneNumber" name="phoneNumber" defaultValue={this.props.modalData.phoneNumber}></input>
+              <div className='rido'>
+              <input type="checkbox" id="ispremium" name="ispremium" ></input>
+              <label for="ispremium"> Premium</label> <br/>
+
+              <input onClick={this.props.handleChange} type="radio" id="forRent" name="status" value="forRent" ></input>
+              <label for="status">for Rent</label>
+
+              <input onClick={this.props.handleChange} type="radio" id="forSale" name="status" value="forSale" ></input>
+              <label for="status"> for Sale</label><br/>
+
+              </div>
+              <button className='btn'> Submit</button>
+            
+            </form>
+          </div>
 
 
-    <button> Submit</button>
-    </form>
-    </Modal.Body>
-        
+        </Modal.Body>
+
         <Modal.Footer>
-       
-        
-          
+
+
+
         </Modal.Footer>
       </Modal>
-      
+
     )
   }
 }
